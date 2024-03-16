382bf3e23b5b9102f3c96aea85683167857741e6
0880880 <98263509+0880880@users.noreply.github.com>
2024-03-16 17:23:55 +0330

    .

--- lwjgl3/src/main/java/com/gameengine/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/gameengine/lwjgl3/Lwjgl3Launcher.java
@@ -1,12 +1,22 @@
 package com.gameengine.lwjgl3;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowAdapter;
+import com.badlogic.gdx.utils.Json;
 import com.gameengine.Main;
+import com.gameengine.api.SerializableProject;
 import com.gameengine.utils.Utils;
+import org.lwjgl.opengl.GL45C;
 
-/** Launches the desktop (LWJGL3) application. */
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
@@ -36,6 +46,7 @@ public class Lwjgl3Launcher {
         configuration.disableAudio(true);
         configuration.useVsync(true);
         configuration.setMaximized(true);
+
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
         configuration.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.GL30, 3, 3);
         return configuration;
